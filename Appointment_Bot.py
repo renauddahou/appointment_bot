e5e47d1e15e3152724f7c9ec2819fa7b813f741c
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-07-30 20:54:29 +0200

    Update Appointment_Bot.py

--- Appointment_Bot.py
+++ Appointment_Bot.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 
 bot = telebot.TeleBot("1935902695:AAFMQQ9Mp7hEsgt-urO-6B29sokLc9yrSuA")  # Insert your bot's TOKEN here
-driver = webdriver.Chrome("~/chromedriver/chromedriver")  # Address browser driver (smth.exe)
+driver = webdriver.Chrome()  # Address browser driver (smth.exe)
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 otp_automate = True  # It is defined to specify the OTP extraction's method
 
